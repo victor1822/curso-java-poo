95c97f7eed1a5f76854bc1a2e1b332df353ebaa7
victor1822 <30930332+victor1822@users.noreply.github.com>
2019-10-31 12:20:32 -0300

    Update Triangle.java

--- src/entities/Triangle.java
+++ src/entities/Triangle.java
@@ -1,14 +1,14 @@
 package entities;
 
 public class Triangle {
-	public double a;
+	public double a; //lados do triângulo 
 	public double b;
 	public double c;
 	
-	public double area() {
-		double p = (a + b + c) / 2.0;
+	public double area() { //Método que calcula a área do triângulo
+		double p = (a + b + c) / 2.0; 
 		
-		double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+		double area = Math.sqrt(p * (p - a) * (p - b) * (p - c)); 
 		
 		return area;
 	}
